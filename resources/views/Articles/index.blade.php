70ae0a184685489bc9bee6f8d9aad5d72b6e19d3
haisenheim <alliages.technologies@gmail.com>
2026-07-11 15:56:41 +0100

    first commit

--- resources/views/Articles/index.blade.php
+++ resources/views/Articles/index.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <title>Document</title>
 </head>
 <body>
-    <h1>LISTE DES ARTICLES</h1>
-    <table style="border: 1px #ccc">
+    <h1 class="text-danger">LISTE DES ARTICLES</h1>
+    <table class="table table-bordered">
         <thead>
             <tr style="border: 1px #ccc solid">
                 <th>ID</th>
